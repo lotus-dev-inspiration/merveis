a8cc022427ae8bacf4fc7673280a5a16b54c4eb8
Vlad Shyrinov <vlad.shyrinov@gmail.com>
2017-05-15 00:09:00 +0300

    search and show-more in collections.html are done

--- src/js/collections.js
+++ src/js/collections.js
@@ -1,52 +1,88 @@
 $(function(){
+
+	// change_img_function //
+
 	var collect_img_item = $(".content_item");
+	var collect_cur_price;
 	collect_img_item.hover(
 		function() {
 			$(this).find("img").attr("src","dist/img/006.jpg");
-			$(this).find(".price").after("<a href='col_detailed.html' class='color col_blue'></a>");
-			$(this).find(".price").remove();
+			// collect_cur_price = $(this).find(".price").html();
+			// $(this).find(".price").empty();
+			// $("<a href='col_detailed.html' class='color col_blue'></a>").appendTo($(this).find(".price"));
 		},
 		function() {
 			$(this).find("img").attr("src","dist/img/005.jpg");
-			$(this).find(".color").after("<p class='price'>1300 грн</p>");
-			$(this).find(".color").remove();
+			// $(this).find(".price").empty();
+			// $(this).find(".price").html(collect_cur_price);
 		}
 		);
 
-	var items_block = '<div class="items_block"><div class="content_item"><div class="item_img"><a href="#"><img src="dist/img/005.jpg"></a></div><div class="item_info"><p>Square T-Shirt Mint</p><p class="price">1300 грн</p></div></div><div class="content_item"><div class="item_img"><a href="#"><img src="dist/img/005.jpg"></a></div><div class="item_info"><p>Square T-Shirt Mint</p><p class="price">1300 грн</p></div></div><div class="content_item"><div class="item_img"><a href="#"><img src="dist/img/005.jpg"></a></div><div class="item_info"><p>Square T-Shirt Mint</p><p class="price">1300 грн</p></div></div></div>';
+	// end_of_change_img_function //
 
-	var collect_show_more = $(".show_more");
-	collect_show_more.on("click", function() {
-		collect_show_more.before(items_block);
-	});
-
-	var count = 0;
-	var count2 = 0;
+	// filter_show_function //
+	var collect_count = 0;
 
 	$(".filter").on("click", function() {
-		if(count%2 == 0) {
+		if(collect_count%2 == 0) {
 			$(".filter_items").show(1000);
-			count++;
+			collect_count++;
 		} else {
 			$(".filter_items").hide(1000);
-			count++;
+			collect_count++;
 		}
 	});
 
-	$(".search_link").on("click", function() {
-		if(count2%2 == 0) {
-			$("#search_input").show(2000);
-			$(".search").css({
-				"right":"8%"
-			});
-			count2++;
-		} else {
+	// end_of_filter_show_function //
+
+	// search_function //
+
+
+	$(".img_hide").on("click", function() { 
+		$("#search_input").show(2000); 
+		$(this).css("display","none");
+		$(".search").css({
+			"right":"8%"
+		});
+	});
+
+	$("search_not_out").on("click", function() { 
+		e.stopPropagation();
+	});
+
+
+	$(".main, .menu, .logo, footer").on("click", function() {
 			$("#search_input").hide(2000);
 			$(".search").css("right","2%");
-			count2++;
+			$(".img_hide").css("display","block");
+	});
+
+	// end_of_search_function //
+
+	// show_more_function //
+
+	var collect_content_item = $(".content_item");
+
+	var i = 9;
+
+	for(; i<collect_content_item.length; i++) {
+		$(collect_content_item[i]).css("display","none");
+	}
+
+	i = 9;
+
+
+	$(".show_more").on("click", function() {
+		for(var j=0; j<3; j++) {
+			$(collect_content_item[i]).css("display","inline-block");
+			i++;
+		}
+
+		if(collect_content_item.length == i) {
+			$(".show_more").remove();
 		}
 	});
 
+	// end_of_show_more_function //
 });
-
 
